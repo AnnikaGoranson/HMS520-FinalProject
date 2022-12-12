68ecc86374dbebeeeee3161d859963c907d31b90
jklusty <jklusty@uw.edu>
2022-12-11 18:27:33 -0800

    Updated maps files

--- 0_data_cleaning.R
+++ 0_data_cleaning.R
@@ -1,7 +1,7 @@
 ## HMS 520 Final Project, Autumn 2022
 ## EPA Toxics Release Inventory data
 ## Authors: Annika Goranson, Caroline Kasman, Jessica Klusty
-## Updated: December 2, 2022
+## Updated: December 11, 2022
 
 
 # load packages
@@ -13,25 +13,55 @@ library(usmap)
 
 ## load data sets -----------------------------------------------------------
 
-# overall source site: https://www.epa.gov/toxics-release-inventory-tri-program/tri-basic-data-files-calendar-years-1987-present
+# overall source site for TRI data
+# https://www.epa.gov/toxics-release-inventory-tri-program/tri-basic-data-files-calendar-years-1987-present
 # 2021 data: 
 tri21 <- read.csv("tri2021.csv", header = TRUE)
 # 1987 data:
 tri87 <- read.csv("tri1987.csv", header = TRUE)
 
-`2021toxinsdata` <- `2021toxinsdata`[,-c(2:3, 10:16, 18:19, 21:33, 35:38, 46)]
+# cancer mortality data from 2020
+# https://www.cdc.gov/nchs/pressroom/sosmap/cancer_mortality/cancer.htm
+cancer_mort20 <- read.csv("cancer_mortality_2020.csv", header = TRUE)
 
 
-`2021toxinsdata`[,c(88)][is.na(`2021toxinsdata`[,c(88)])] <- 0
+## clean data ---------------------------------------------------------------
 
-`2021toxinsdata`[is.na(`2021toxinsdata`)] <- "Not specified"
+# create function to clean data sets
+# provide: TRI data set from EPA website, unedited
+clean_tri <- function(tri) {
+  
+  # remove unnecessary columns
+  tri <- tri[,-c(2:3, 10:16, 18:19, 21:33, 35:38, 46)]
+  
+  # remove NAs
+  tri[,c(88)][is.na(tri[,c(88)])] <- 0
+  tri[is.na(tri)] <- "Not specified"
+  
+  # rename columns
+  tri <- rename(tri, c("year" = 1, "facility.name" = 2, "street.address"= 3, 
+                       "city"=4, "county"=5, "state"=6, "zip"=7, 
+                       "stand.co.name" = 8, "industry.sector" = 9, 
+                       "chemical" = 10, "clean.air.act" = 11, 
+                       "classification" = 12, "metal" = 13, 
+                       "metal.category" = 14, "carcinogen" = 15, "pbt" = 16, 
+                       "pfas" = 17, "unit.of.measure" = 18, 
+                       "production.ratio" = 90))
+  
+  # assign "yes" and "no" to numerical values
+  tri[tri =='YES'] <- 1
+  tri[tri =='NO'] <- 0
+  
+  return(tri)
+}
 
-`2021toxinsdata` <- rename(`2021toxinsdata`, c("year" = 1, "facility.name" = 2, "street.address"= 3, "city"=4, 
-                                               "county"=5, "state"=6, "zip"=7, "stand.co.name" = 8, "industry.sector" = 9, 
-                                               "chemical" = 10, "clean.air.act" = 11, "classification" = 12, "metal" = 13,
-                                               "metal.category" = 14, "carcinogen" = 15, "pbt" = 16, "pfas" = 17, 
-                                               "unit.of.measure" = 18, "production.ratio" = 90))
-                                               
-`2021toxinsdata`[`2021toxinsdata`=='YES'] <- 1
-`2021toxinsdata`[`2021toxinsdata`=='NO'] <- 0
+# clean data
+tri21 <- clean_tri(tri21)        
+tri87 <- clean_tri(tri87)
 
+`2021toxinsdata` <- tri21
+
+
+## pull just 2020 data from cancer mortality data
+cancer_mort20 <- rename_with(cancer_mort20, tolower)
+cancer_mort20 <- filter(cancer_mort20, year == 2020)
--- 1_maps.R
+++ 1_maps.R
@@ -1,44 +1,81 @@
 ## HMS 520 Final Project, Autumn 2022
 ## EPA Toxics Release Inventory data
 ## Authors: Annika Goranson, Caroline Kasman, Jessica Klusty
-## Updated: December 2, 2022
+## Updated: December 11, 2022
 
 
 ## number of incidents by state ---------------------------------------------
 
-# determine number of incidents by state for each year
-inc_st21 <- as.data.frame(table(tri21$X8..ST))
-colnames(inc_st21) <- c("state", "freq")
-inc_st87 <- as.data.frame(table(tri87$X8..ST))
-colnames(inc_st87) <- c("state", "freq")
+## create function to map number of incidents by state
+## input: tri = TRI data set from EPA, cleaned in 0_data_cleaning.R
+##        scale = optional scale max value, set to 8500 by default
+map_incidents <- function(tri, scale = 8500) {
+  
+  # determine number of incidents by state for each year
+  inc_st <- as.data.frame(table(tri$state))
+  colnames(inc_st) <- c("state", "freq")
+  
+  # plot map
+  incident_map <- plot_usmap(data = inc_st, values = "freq") +
+    scale_fill_continuous(low = "white", high = "dark green", 
+                          name = "Incidents", limits = c(0, scale), 
+                          label = scales::comma) + 
+    theme(legend.position = "right")
+  
+  return(incident_map)
+}
 
 # 2021 map
-plot_usmap(data = inc_st21, values = "freq") +
-  scale_fill_continuous(low = "white", high = "dark green", name = "Incidents",
-                        limits = c(0, 8500), label = scales::comma) + 
-  theme(legend.position = "right") +
-  ggtitle("TRI Incidents in 2021")
-
+map_incidents(tri21) + ggtitle("TRI Incidents in 2021")
 # 1987 map
-plot_usmap(data = inc_st87, values = "freq") +
-  scale_fill_continuous(low = "white", high = "dark green", name = "Incidents",
-                        limits = c(0, 8500), label = scales::comma) + 
-  theme(legend.position = "right") +
-  ggtitle("TRI Incidents in 1987")
+map_incidents(tri87) + ggtitle("TRI Incidents in 1987")
 
+
+## create function to test for significant difference in incidents between years
+## input: two TRI data sets from EPA, cleaned in 0_data_cleaning.R,
+##        earlier year first
+inc_diff <- function(tri_1, tri_2) {
+  
+  # combine data sets
+  inc_st_both <- merge(
+    as.data.frame(table(tri_1$state)),
+    as.data.frame(table(tri_2$state)),
+    by = c("Var1"),
+    suffix = c("_1", "_2"),
+    all.x = TRUE
+  )
+  
+  # set up a paired t-test for differences
+  inc_diff <- t.test(inc_st_both$Freq_1, inc_st_both$Freq_2, paired = TRUE)
+  
+  return(inc_diff)
+  
+}
 
 ## significant difference between number of incidents in 1987 vs 2021?
-# combine data sets
-inc_st_both <- merge(
-  inc_st87,
-  inc_st21,
-  by = c("state"),
-  suffix = c("_87", "_21"),
-  all.x = TRUE
-)
-# set up a paired t-test for differences
-t.test(inc_st_both$freq_87, inc_st_both$freq_21, paired = TRUE)
+inc_diff(tri87, tri21)
 
 
 ## number of carcinogenic incidents by state --------------------------------
 
+# carcinogenic incidents by state in 2021
+map_incidents(filter(tri21, carcinogen == 1), scale = 2500) + 
+  ggtitle("Carcinogenic TRI Incidents in 2021")
+# in 1987
+map_incidents(filter(tri87, carcinogen == 1), scale = 2500) + 
+  ggtitle("Carcinogenic TRI Incidents in 1987")
+
+## significant difference between number of carcinogenic incidents 
+## in 1987 vs 2021?
+inc_diff(filter(tri87, carcinogen == 1), filter(tri21, carcinogen == 1))
+
+
+## map carcinogenic incidents vs cancer mortality rate ----------------------
+
+# map cancer mortality rates by state in 2020
+plot_usmap(data = cancer_mort20, values = "rate") +
+  scale_fill_continuous(low = "white", high = "dark green", 
+                        name = "Mortality rate per 100k", 
+                        limits = c(110, 180), label = scales::comma) + 
+  theme(legend.position = "right") +
+  ggtitle("Cancer mortality rates by state in 2020")
