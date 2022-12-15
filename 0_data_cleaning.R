fa8ead62a710bb393bad61e4f200a45800fe8922
ckas78 <114685178+ckas78@users.noreply.github.com>
2022-12-15 14:37:19 -0800

    updating descriptions, adding column to rename

--- 0_data_cleaning.R
+++ 0_data_cleaning.R
@@ -13,7 +13,7 @@ library(usmap)
 
 ## load data sets -----------------------------------------------------------
 
-# overall source site for TRI data
+# overall source site for TRI (Toxic Release Inventory) data
 # https://www.epa.gov/toxics-release-inventory-tri-program/tri-basic-data-files-calendar-years-1987-present
 # 2021 data: 
 tri21 <- read.csv("tri2021.csv", header = TRUE)
@@ -32,27 +32,39 @@ cancer_mort20 <- read.csv("cancer_mortality_2020.csv", header = TRUE)
 clean_tri <- function(tri) {
   
   # remove unnecessary columns
-  tri <- tri[,-c(2:3, 10:16, 18:19, 21:33, 35:38, 46)]
-  
-  # remove NAs
-  tri[,c(88)][is.na(tri[,c(88)])] <- 0
-  tri[is.na(tri)] <- "Not specified"
-  
-  # rename columns
-  tri <- rename(tri, c("year" = 1, "facility.name" = 2, "street.address"= 3, 
-                       "city"=4, "county"=5, "state"=6, "zip"=7, 
-                       "stand.co.name" = 8, "industry.sector" = 9, 
-                       "chemical" = 10, "clean.air.act" = 11, 
-                       "classification" = 12, "metal" = 13, 
-                       "metal.category" = 14, "carcinogen" = 15, "pbt" = 16, 
-                       "pfas" = 17, "unit.of.measure" = 18, 
-                       "production.ratio" = 90))
-  
-  # assign "yes" and "no" to numerical values
-  tri[tri =='YES'] <- 1
-  tri[tri =='NO'] <- 0
-  
-  return(tri)
+        tri <- tri[,-c(2:3, 10:16, 18:19, 21:33, 35:38, 46)]
+        
+        # remove NAs
+        tri[,c(88)][is.na(tri[,c(88)])] <- 0
+        tri[is.na(tri)] <- "Not specified"
+        
+        # rename columns - using column indices
+        tri <- rename(tri, c("year" = 1, 
+                             "facility.name" = 2, 
+                             "street.address"= 3, 
+                             "city"=4, 
+                             "county"=5, 
+                             "state"=6, "zip"=7, 
+                             "stand.co.name" = 8, 
+                             "industry.sector" = 9, 
+                             "chemical" = 10, 
+                             "clean.air.act" = 11, 
+                             "classification" = 12, 
+                             "metal" = 13, 
+                             "metal.category" = 14, 
+                             "carcinogen" = 15, 
+                             "pbt" = 16, 
+                             "pfas" = 17, 
+                             "unit.of.measure" = 18,
+                             "onsite_release_total" = 62,
+                             "production.ratio" = 90
+                             )
+                      )
+        # assign "yes" and "no" to numerical values
+        tri[tri =='YES'] <- 1
+        tri[tri =='NO'] <- 0
+        
+        return(tri)
 }
 
 # clean data
